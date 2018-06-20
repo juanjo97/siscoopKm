aa2873e3d321ce739c0fa9ca67ddd7ef7e4e0fb7
juanjo97 <40218735+juanjo97@users.noreply.github.com>
2018-06-20 18:38:38 -0500

    peticion http para la validacion de un usuario

--- src/app/login/shared/authentication.service.ts
+++ src/app/login/shared/authentication.service.ts
@@ -1,3 +1,20 @@
 import {Injectable} from'@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 
+import { Observable } from 'rxjs';
+import { LoginObject } from './login-object.model';
+import { Session } from '../../core/models/session.model'
+
+@Injectable() export class AuthenticationService {
+ constructor(private http: Http) {}
+ private basePath = '/api/authenticate/';
+ login(loginObj: LoginObject): Observable<Session> {
+ return this.http.post(this.basePath + 'login', loginObj).map(this.extractData);
+ }
+ logout(): Observable<Boolean> {
+ return this.http.post(this.basePath + 'logout', {}).map(this.extractData);
+ }
+ private extractData(res: Response) {
+ let body = res.json();
+ return body;
+ }
+}
